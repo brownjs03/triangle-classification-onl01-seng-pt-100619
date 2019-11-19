93f553639853f960111f6a8b855a6464d17ef156
Jeffrey Brown <brownjs03@gmail.com>
2019-11-19 05:07:36 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,10 +15,14 @@ attr_accessor :side_1, :side_2, :side_3
       :isosceles
     elsif @side_1 != @side_2 || @side_2 != @side_3 || @side_1 != @side_3
       :scalene 
-    elsif @side_1 == 0 
+    elsif @side_1 == 0 || @side_2 == 0 || @side_3 == 0 
+    TriangleError
     end 
+   end
    
-  end
+   class TriangleError < StandardError 
+     
+   end 
   
 end
 
