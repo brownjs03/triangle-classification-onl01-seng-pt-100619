90b70ebc45f15dd98d858e1aa8d7419ecf7a68a1
Jeffrey Brown <brownjs03@gmail.com>
2019-11-19 05:10:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ attr_accessor :side_1, :side_2, :side_3
   def kind 
     if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0 
     raise TriangleError
-      @side_1 == @side_2 && @side_2 == @side_3 
+    else @side_1 == @side_2 && @side_2 == @side_3 
       :equilateral 
     elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3 
       :isosceles
