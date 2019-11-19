55b5d2881ce4f9fd48f49ea50c3fa41d164b85f7
Jeffrey Brown <brownjs03@gmail.com>
2019-11-19 05:05:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,8 @@ attr_accessor :side_1, :side_2, :side_3
       :equilateral 
     elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3 
       :isosceles
-    else 
+    elsif @side_1 != @side_2 || @side_2 != @side_3 || @side_1 != @side_3
+      :scalene 
     end 
    
   end
